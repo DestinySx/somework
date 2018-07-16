3be31f354f3836df11670bd5a8c2ae47fb41069c
suxin <981828064@qq.com>
2018-07-16 20:46:38 +0800

    模板模式

--- template/src/study/pattern/template/MemberDaoTest.java
+++ template/src/study/pattern/template/MemberDaoTest.java
@@ -2,6 +2,10 @@ package study.pattern.template;
 
 import study.pattern.template.dao.MemberDao;
 
+/*
+ * time 20180716
+ * author suxin
+ * */
 public class MemberDaoTest {
     public static void main(String[] args) {
         MemberDao memberDao = new MemberDao();
--- template/src/study/pattern/template/RowMapper.java
+++ template/src/study/pattern/template/RowMapper.java
@@ -2,6 +2,10 @@ package study.pattern.template;
 
 import java.sql.ResultSet;
 
+/*
+ * time 20180716
+ * author suxin
+ * */
 public interface RowMapper<T> {
     public T mapRow(ResultSet rs,int rowNum) throws Exception;;
 
